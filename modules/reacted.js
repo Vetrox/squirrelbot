90c2a27a91450be4e74641c6efa8d358488564b6
Vetrox <fudarux@gmail.com>
2020-10-26 13:25:12 +0100

    before database port to json

--- modules/reacted.js
+++ modules/reacted.js
@@ -1,17 +1,21 @@
 const discord       = require('discord.js');
 const { prefix }    = require('../config.json');
-const attributes = {modulename : 'reacted', commands : ['add', 'remove']};
+const attributes = {modulename : 'reacted', commands : ['add', 'show']};
 
 
 function help(channel){
     channel.send(`Managed Nachrichten, mit denen ein User sich Rollen geben kann.
-!reacted work_here
-	Beobachtet alle Nachrichten in dem channel. Wenn auf eine Nachricht reagiert wurde, weist der Bot der Person die Rolle zu,
-		welche ihr mit !reacted assign zugewiesen wurde.
-!reacted assign <emoji> <role>'
-	Weist dem emoji die beschriebene Rolle zu.
-	- Wenn die Methode ohne parameter aufgerufen wird zeigt sie alle Zuweisungen an
-	`);
+!reacted add <messageID> <emoji> <role> [<emoji_1 <role_1> ...] [-required <role> [<role2> ...]]'
+	messageID: Um die MessageID von einer Nachricht zu bekommen, musst du den Developer-Modus in Discord aktivieren
+		und anschließend auf deine Nachricht klicken.
+	Nach messageID kommen Paare von je einem Emoji und einer Rolle, welche miteinander verknüpft werden.
+	-required: Optionaler parameter. Falls nur Bestimmte Rollen die Möglichkeit haben sollen, auf die Message zu reacten.
+!reacted show <messageID>
+	Zeigt die Informationen (Metadata) zu der Nachricht an.
+	messageID: Die MessageID, welche in der Einbettung der bot-Nachricht angezeigt wird.
+!reacted remove <messageID>
+	Löscht die Rollenzuweisungsnachricht auch Serverseitig, was zur Vorbeugung von Problemen mit der Datenbank dient.
+`);
 }
 
 function initialize(){
@@ -25,11 +29,25 @@ async function onMessage(message) {
 
     switch(split[1]){
     case attributes.commands[0]:
-		//listen in this channel.
-		/*let ch_id = message.channel.id.toString(); 
-		bot['api'].database_create_if_not_exists(attributes.modulename + '_listening', ['guild_id', 'channel_id']); //lässt nur einen channel der Rollenvergabe zu.
-    	let bot['api'].lookup_key(attributes.modulename + '_listening', 'guild_id', message.guild.id.toString());
-    	let index = 
+    /*
+    	if(!split[2] || split.length < 5) help(message.channel);
+    	let assigns_list = [];
+    	let required_roles = [];
+    	for(let req = false, i = 3; i < split.length; i++){
+    		if(req == false && split[i] === '-required') req = true;
+    		if(req == false) {
+    			assigns_list.push(split[i]);
+    		}else if(req == true){
+    			required_roles.push(split[i]);
+    		}else{
+    			throw Error;
+    		}
+    	}
+    	let role 
+    	if(assigns_list.length % 2 != 0) throw Error;
+    	for(let i = 0; i < split.length / 2; i+=2){
+
+    	}
         */
         break;
     case attributes.commands[1]:
