6e8a0e1b0963a0888c13341913c3b51ff37537cc
Vetrox <fudarux@gmail.com>
2021-01-18 13:22:57 +0100

    Annotating Parameter class in api.js

--- api.js
+++ api.js
@@ -24,6 +24,8 @@ const wait = require("util").promisify(setTimeout); //async wait
  *
  * this.index: Is an index for each key which has the following structure.
  * array[dict{value1 : index/row in this.data, ...},...]
+ *
+ * @author Felix Ludwig
  */
 class Database {
 	/**
@@ -267,10 +269,6 @@ class Database {
 	}
 
 	/**
-	Writes the data on disk (async)
-	(optional callback)
-**/
-	/**
 	 * Writes the data asynchronously on disc.
 	 *
 	 * @param callback the callback function, when successfully saved.
@@ -329,7 +327,39 @@ class Database {
 	}
 }
 
+/**
+ * A Parameter object is used to store information about one single parameter of a command.
+ * It stores crucial information about:
+ * - it's type: Which can be 'required' or 'optional'. If the type is 'required' it's not possible to execute the
+ * command without this parameter set.
+ * - its' dependent parameters: A parameter can require other parameters to be set. If the other parameters aren't
+ * set by the user and they have the default_construct set to true, the default_args are written as arguments for
+ * the required parameter. Otherwise an error will be thrown, urging the user to provide the required parameter with
+ * it's values.
+ * - it's arg_check_lambda: Currently there is no possibility to check for anything else than the number of
+ * arguments provided to a parameter. This lambda should return true, when the number of arguments matches the
+ * expected number of arguments. As an example you could check, if there is an even number of arguments (and they are
+ * at least 2) with the following lambda. (nr) => nr >= 2 && nr % 2 == 0
+ * - it's default args: If the parameter can be self constructed this array specifies the arguments to be set.
+ * - Whether it can be self constructed (default construct)
+ *
+ * @see Command
+ *
+ * @author Felix Ludwig
+ */
 class Parameter {
+	/**
+	 * Constructor of the Parameter-class.
+	 *
+	 * @param parname the name of the parameter starting with a minus.
+	 * @param type either 'required' or optional
+	 * @param dependent_params an array of parameter-names this parameter depends on to be set
+	 * @param description to be shown to the user
+	 * @param arg_check_lambda the lambda to check for the number of arguments provided by the user
+	 * @param default_args an array of arguments to be set, if default_construct is true and either 'required' is the
+	 * type of this parameter OR another parameter depends on this parameter and needs to construct this.
+	 * @param default_construct whether it should be possible to self construct this parameter with the default_args.
+	 */
 	constructor(
 		parname,
 		type,
