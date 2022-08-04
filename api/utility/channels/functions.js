35ffb69c939b3d815fd3e3c17b6ff3e34a87a660
Vetrox <39677514+Vetrox@users.noreply.github.com>
2022-08-04 21:49:04 +0200

    channels types are now numbers

--- api/utility/channels/functions.js
+++ api/utility/channels/functions.js
@@ -19,9 +19,31 @@ const err = require.main.require("./api/errors/errors");
 function channel_check(channel, req_list) {
 	if (!channel) throw new err.BotError("Channel war nicht definiert.");
 	for (let req of req_list) {
-		if (["dm", "text", "voice", "category", "news", "store"].indexOf(req) === -1)
+		if (["dm", "text", "voice", "category", "news"].indexOf(req) === -1)
 			throw new err.BotError(`Der gesuchte Channel-Typ ${req} ist invalide.`);
-		if (channel.type === req) return true;
+        let id = 0;
+        switch (req) {
+            case "dm":
+                id = 1;
+                break;
+            case "text":
+                id = 0;
+                break;
+            case "voice":
+                id = 2;
+                break;
+            case "category":
+                id = 4;
+                break;
+            case "news":
+                id = 5;
+                break;
+        }
+        console.log(req);
+        console.log(channel.type);
+        console.log(id);
+        console.log("--");
+		if (channel.type === id) return true;
 	}
 	return false;
 }
